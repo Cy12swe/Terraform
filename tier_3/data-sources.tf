0b6260091bc87022441acfd9479af6a127975941
Cy12swe <andreisagum@gmail.com>
2023-02-28 01:12:00 +0800

    Added tier 1,2,3

---
+++ tier_3/data-sources.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+data "aws_availability_zones" "available" {
+}
+
+data "aws_region" "current" {}
+
+# Define VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_key_pair" "Cyrus" {
+  key_name = "Cyrus"
+}
+
+variable "private_subnets" {
+  default = {
+    "cp_301_302_sg_prod_v3"  = 220
+  }
+}
---
+++ tier_3/main.tf
@@ -0,0 +1,51 @@
+
+# Private Subnets
+resource "aws_subnet" "private_subnets" {
+  for_each   = var.private_subnets
+  vpc_id     = data.aws_vpc.default.id
+  cidr_block = cidrsubnet(data.aws_vpc.default.cidr_block, 8, each.value)
+
+  tags = {
+    Terraform = true
+  }
+}
+
+# Security Groups
+resource "aws_security_group" "cp_301_302_sg_prod_v3" {
+  name        = "cp_301_302_sg_prod_v3"
+  description = "Allow inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+  ingress {
+    description = "SSH Inbound"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["172.31.250.0/24", "120.29.76.169/32"]
+  }
+  egress {
+    description = "Global Outbound"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name    = "cp_301_302_sg_prod_v3"
+    Purpose = "For dev"
+  }
+}
+
+resource "aws_instance" "cp_301_302_sg_prod_v3" {
+  ami                         = "ami-06e85d4c3149db26a"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.private_subnets["cp_301_302_sg_prod_v3"].id
+  security_groups             = [aws_security_group.cp_301_302_sg_prod_v3.id]
+  associate_public_ip_address = true
+  key_name                    = data.aws_key_pair.Cyrus.key_name
+  #   iam_instance_profile        = "CloudWatchAgentServerPolicy"
+
+  tags = {
+    Name = "cp_301_302-prod_v3"
+  }
+}
+
